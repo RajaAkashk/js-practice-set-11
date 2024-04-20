8c53dea2266150f351fc3ffd41920209783bd08a
Raja Akash Kumar <159010538+RajaAkashk@users.noreply.github.com>
2024-04-20 14:37:50 +0530

    js

--- app.js
+++ app.js
@@ -105,3 +105,139 @@ function printDivisibleNUmbersBetween(p,n,m){
 }
 
 printDivisibleNUmbersBetween(2,3,9)
+
+
+console.log("A5.4_CW")
+console.log("---- ---- ----")
+console.log("1.1")
+console.log("---- ---- ----")
+
+function SumUpToN(n){
+  let sum=0
+  for(let i=1;i<=n;i++){
+    sum=sum+i
+  }
+  return sum
+}
+console.log(SumUpToN(5))
+
+
+console.log("---- ---- ----")
+console.log("1.2")
+console.log("---- ---- ----")
+
+
+function sumOfOddNUmbersUpToN(n){
+  let sum=0
+  for(let i=1;i<=n;i++){
+    if(i%2 !== 0){
+      sum=sum+i
+    }
+  }return sum
+}
+console.log(sumOfOddNUmbersUpToN(7))
+
+
+console.log("---- ---- ----")
+console.log("1.3")
+console.log("---- ---- ----")
+
+
+function sumDivisibleByThreeAndSevenUpToN(n){
+  let sumOfNumbersDivisibleBy3=0
+  let sumOfNumbersDivisibleBy7=0
+
+for(let i=1;i<=n;i++){
+  
+  if(i%3===0){
+sumOfNumbersDivisibleBy3 = sumOfNumbersDivisibleBy3 + i
+  }
+  if(i%7===0){
+    sumOfNumbersDivisibleBy7 = sumOfNumbersDivisibleBy7 + i
+  }
+}
+  console.log("sum Of Numbers Divisible By 3:",sumOfNumbersDivisibleBy3)
+  console.log("sum Of Numbers Divisible By 7:",sumOfNumbersDivisibleBy7)
+}
+sumDivisibleByThreeAndSevenUpToN(21)
+
+
+console.log("---- ---- ----")
+console.log("1.4")
+console.log("---- ---- ----")
+
+
+function factorial(n){
+  let result=1;
+  for(let i=1;i<=n;i++){
+    result=result*i
+  }
+  return result
+}
+console.log(factorial(5))
+
+console.log("A5.4_HW_2");
+console.log("---- ---- ----");
+console.log("1.1");
+console.log("---- ---- ----");
+
+function averageOfNumbersuptoN(n){
+  let sum=0
+  for(let i=1;i<=n;i++){
+    sum= sum+i;
+  }
+  return sum / n
+}
+
+console.log("The average of numbers from 1 to 10:",averageOfNumbersuptoN(10) );
+
+console.log("---- ---- ----");
+console.log("1.2");
+console.log("---- ---- ----");
+
+function sumOfNumbersGreaterThanSixUpToN(n){
+  let result=0
+  for(let i=1;i<=n;i++){
+    if(i>6){
+      result=result+i;
+    }
+  } return result
+} 
+console.log("The sum of numbers is:",sumOfNumbersGreaterThanSixUpToN(12));
+
+console.log("---- ---- ----");
+console.log("1.3");
+console.log("---- ---- ----");
+
+function sumDivisibleByEightUpToN(n){
+  let result=0
+  for(let i=10;i<=n;i++){
+    if(i % 8 === 0){
+      result=result+i
+    }
+  } return result
+} 
+
+console.log(sumDivisibleByEightUpToN(40))
+
+console.log("---- ---- ----");
+console.log("1.4");
+console.log("---- ---- ----");
+
+function sumEvenAndOddUpToN (n){
+  let sumOfAllEvenNumbers=0 
+  let sumOfAllOddNumbers=0 
+
+for(let i=1;i<=n;i++){
+  if(i % 2 ===0){
+    sumOfAllEvenNumbers = sumOfAllEvenNumbers +i 
+  }
+  if(i % 2 !==0){
+    sumOfAllOddNumbers = sumOfAllOddNumbers + i
+  }
+}
+  console.log("Sum Of All Even Numbers:",sumOfAllEvenNumbers)
+  console.log("Sum Of All Odd Numbers:",sumOfAllOddNumbers)
+}
+
+sumEvenAndOddUpToN (30)
